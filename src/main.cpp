7978fc1e21a525c0774b7ee1541d665063b29c5e
Jeremy Trufier <jeremy@trufier.com>
2019-07-04 15:02:24 +0200

    fix: fix line detections

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ const uint8_t SOUND_PIN = A5;
 
 const uint8_t DART_MASTER_LINE_SIZE = 10;
 const uint8_t DART_SLAVE_LINE_SIZE = 7;
-const uint8_t INPUT_MATRIXES[ANALOG_DART_PIN_SIZE][3][DART_MASTER_LINE_SIZE] = {{
-    { 118, 115, 113, 112, 110, 108, 106, 104, 97, 91 },
+const int INPUT_MATRIX[ANALOG_DART_PIN_SIZE][3][DART_MASTER_LINE_SIZE] = {{
+    { 118, 116, 114, 112, 110, 108, 106, 104, 97, 91 },
     { 70, 64, 59, 54, 50, 47, 44, 42, 33, 27 },
     { 23, 20, 17, 15, 13, 12, 11, 10, 7, 6 }
   }, {
@@ -20,10 +20,12 @@ const uint8_t INPUT_MATRIXES[ANALOG_DART_PIN_SIZE][3][DART_MASTER_LINE_SIZE] = {
   }
 };
 
-const uint8_t INPUT_MATRIXES_COEFF = 8; // meaning 1024 (HIGH VOLTAGE) input will become 128
+const uint8_t INPUT_MATRIX_GROUP_SIZES[ANALOG_DART_PIN_SIZE] = { 3, 2, 2 };
+
+const uint8_t INPUT_MATRIX_COEFF = 8; // meaning 1024 (HIGH VOLTAGE) input will become 128
 
 // Ignore unrelevant values, < to this
-const uint8_t INPUT_MATRIXES_THREASHOLD = 2 * INPUT_MATRIXES_COEFF;
+const uint8_t INPUT_MATRIX_THREASHOLD = 2 * INPUT_MATRIX_COEFF;
 
 
 void play(uint16_t melody[], uint8_t noteDurations[]) {
@@ -49,7 +51,7 @@ void handleLines(uint8_t masterLine, uint8_t slaveLine) {
 
   // convert masterLine + slaveLine to val + multiplicator
 
-  PORTD = (slaveLine + 1) << 3 | (masterLine + 1);
+  PORTD = slaveLine << 4 | masterLine;
 
   if (val) {
     hit(val, multiplicator);
@@ -59,31 +61,34 @@ void handleLines(uint8_t masterLine, uint8_t slaveLine) {
 void handleAnalogMatrix(int inputs[ANALOG_DART_PIN_SIZE]) {
   uint8_t masterLine = 0;
   uint8_t slaveLine = 0;
-
+    
+  int cumSize = 0;
   for(uint8_t i=0; i < ANALOG_DART_PIN_SIZE; i++) {
+    const int handledLineSize = INPUT_MATRIX_GROUP_SIZES[i];
+    cumSize += handledLineSize;
+    
     // Ignore unrelevant inputs
-    if (inputs[i] <= INPUT_MATRIXES_THREASHOLD) continue;
-
-    const size_t handledLineSize = sizeof(INPUT_MATRIXES[i]);
+    if (inputs[i] <= INPUT_MATRIX_THREASHOLD) continue;
 
     int input = inputs[i];
     unsigned int curDiff = -1;
     bool reached = false;
     for(uint8_t j=0; j < handledLineSize; j++) {
       for(uint8_t k = 0; k < DART_MASTER_LINE_SIZE; k++) {
-        unsigned int diff = abs(INPUT_MATRIXES[i][j][k] * INPUT_MATRIXES_COEFF - input);
+        unsigned int diff = abs(INPUT_MATRIX[i][j][k] * INPUT_MATRIX_COEFF - input);
         if (curDiff < 0) {
           curDiff = diff;
         }
 
-        if (diff <= curDiff) {
-          masterLine = k;
-          slaveLine = j * (i + 1);
-        } else {
+        if (diff > curDiff) {
           // we can only go closer, if we start having higher diff, then previous value is correct
           reached = true;
           break;
         }
+
+        masterLine = k + 1;
+        slaveLine = cumSize + j - handledLineSize + 1;
+        curDiff = diff;
       }
       if (reached) break;
     }
@@ -102,9 +107,9 @@ void trigger() {
     inputs[i] = analogRead(analogDartPins[i]);
   }
 
-  PORTD = inputs[0];
+  // PORTD = inputs[0] / 8;
 
-  // handleAnalogMatrix(inputs);
+  handleAnalogMatrix(inputs);
 }
 
 void setup() {
@@ -120,6 +125,7 @@ void setup() {
 
 void loop() {
   trigger();
+  delay(100);
 
   // Just to check if program is working
   // PORTD ^= 0b10000000;
